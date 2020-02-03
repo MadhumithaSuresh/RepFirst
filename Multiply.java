84cf37702b1955c2b76d3f91f9450b2df3c813e0
MadhumithaSuresh <MadhumithaSuresh>
2020-02-03 11:27:43 +0530

    Y

--- Multiply.java
+++ Multiply.java
@@ -1,9 +1,9 @@
-public class JenkinsExample
+public class Multiply
 {
 public static void main(String args[])
 {
-int a=300,b=302;
-int ans=a+b;
+int a=300,b=200;
+int ans=a*b;
 System.out.println(ans+"   Yeah Wow... You did it..Yass ");
 } 
 }
