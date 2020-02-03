ce30bdeb912b556012cda3c42f2c8e9482232b40
MadhumithaSuresh <MadhumithaSuresh>
2020-02-03 11:31:06 +0530

    Y

--- JenkinsExample.java
+++ JenkinsExample.java
@@ -2,7 +2,7 @@ public class JenkinsExample
 {
 public static void main(String args[])
 {
-int a=300,b=302;
+int a=302,b=302;
 int ans=a+b;
 System.out.println(ans+"   Yeah Wow... You did it..Yass ");
 } 
